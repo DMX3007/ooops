8afbf09b42478e5dff8388480f2685d15b3668c9
earielle <demix3991@gmail.com>
2026-09-15 13:20:26 +0700

    feature: complete web server prod and dev build

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ooops
+
+## Запуск web сервера
+
+    Установка зависимостей
+
+    npm ci
+
+    Сборки:
+    
+    - development:
+    
+     npm run dev
+     
+    - production: 
+    
+     npm run prod
+
+    
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const express = require('express')
+
+const app = express()
+const DEFAULT_PORT = 3000
+const port = process.env.PORT || DEFAULT_PORT
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('')
+})
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', message: 'Health check - OK' });
+});
+
+app.listen(port, () => {
+    console.log('web app is listening on port ', port)
+})
